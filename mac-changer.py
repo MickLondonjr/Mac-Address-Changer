44f7e50d9992f903115e0b42b01de456e645621f
parallels <parallels@kali-gnu-linux-2023.2>
2024-08-28 10:21:53 +0700

    Fixed the ifconfig command in mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -9,4 +9,4 @@ print("[+] Changing MAC address for " + interface + " to " + new_mac )
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call([" ifconfig", interface, "up"])
+subprocess.call(["ifconfig", interface, "up"])
